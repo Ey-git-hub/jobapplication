a904b1b188d7251f00491d8289f07b4ed6acc579
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 13:53:33 +0300

    Add POST endpoint to JobController for adding new jobs

--- src/main/java/com/app/jobapplication/Job/JobController.java
+++ src/main/java/com/app/jobapplication/Job/JobController.java
@@ -2,8 +2,11 @@ package com.app.jobapplication.Job;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +16,15 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/jobs")
 @RequiredArgsConstructor
 public class JobController {
-    private final JobService jobService; 
+    private final JobService jobService;
+
     @GetMapping
-    public ResponseEntity<List<JobResponse>> getAllJobs(){
+    public ResponseEntity<List<JobResponse>> getAllJobs() {
         return ResponseEntity.ok(jobService.getAllJobs());
+    }
+
+    @PostMapping
+    public ResponseEntity<JobResponse> addNewJob(@RequestBody JobRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(jobService.addNewJob(request));
     }
 }
