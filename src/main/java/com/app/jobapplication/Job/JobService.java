bafacb263c72b34fbc6895ab887be3f09e4a344c
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 15:10:25 +0300

    Remove unused import statement for Nullable annotation in JobService

--- src/main/java/com/app/jobapplication/Job/JobService.java
+++ src/main/java/com/app/jobapplication/Job/JobService.java
@@ -3,7 +3,7 @@ package com.app.jobapplication.Job;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.jspecify.annotations.Nullable;
+// import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
