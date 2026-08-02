3d96d98fb08b199cc904cd578fe9059742f380de
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 08:30:13 +0300

    Implement JobService class to handle job retrieval logic

---
+++ src/main/java/com/app/jobapplication/Job/JobService.java
@@ -0,0 +1,19 @@
+package com.app.jobapplication.Job;
+
+// import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JobService {
+    private final JobRepository jobRepository;
+
+
+    public JobResponse getAllJobs() {
+       jobRepository.findAll().stream()
+       .map(JobResponse::fromEntity)
+       .collect(Collectors.toList);
+        
+    }
+    
+}
