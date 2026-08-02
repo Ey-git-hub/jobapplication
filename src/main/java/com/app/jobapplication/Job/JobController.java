e46f0115f339c8c76af3ca07dcdae885331a6903
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 14:55:15 +0300

    Refactor JobController to correct getJobById endpoint and implement updateJob method

--- src/main/java/com/app/jobapplication/Job/JobController.java
+++ src/main/java/com/app/jobapplication/Job/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,8 +29,12 @@ public class JobController {
     public ResponseEntity<JobResponse> addNewJob(@RequestBody JobRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(jobService.addNewJob(request));
     }
-    @GetMapping("/id")
-    public ResponseEntity<JobResponse> getJobId(@PathVariable Long id){
-        return ResponseEntity.ok(jobService.getJobById());
+    @GetMapping("/{id}")
+    public ResponseEntity<JobResponse> getJobById(@PathVariable Long id) {
+        return ResponseEntity.ok(jobService.getJobById(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<JobResponse> updateJob(@PathVariable Long id,@RequestBody JobRequest request) throws IllegalAccessException{
+        return ResponseEntity.ok(jobService.updateJob(id,request));
     }
 }
