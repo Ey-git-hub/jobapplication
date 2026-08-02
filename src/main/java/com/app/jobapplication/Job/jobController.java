f3b54a382d042e8cd2a4dadd18f12719664c0792
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 08:29:02 +0300

    Implement jobController to handle job retrieval API

--- src/main/java/com/app/jobapplication/Job/jobController.java
+++ src/main/java/com/app/jobapplication/Job/jobController.java
@@ -1,8 +1,21 @@
 package com.app.jobapplication.Job;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/api/v1/jobs")
+@RequiredArgsConstructor
 public class jobController {
-    
+    private final JobService jobService; 
+    @GetMapping
+    public ResponseEntity<List<JobResponse>> getAllJobs(){
+        return ResponseEntity.ok(jobService.getAllJobs());
+    }
 }
