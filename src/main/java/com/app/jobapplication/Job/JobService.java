a706e8fc50ea1649c0ed2099fc4234913653b4f2
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 11:22:23 +0300

    Refactor getAllJobs method in JobService to return a List of JobResponse objects

--- src/main/java/com/app/jobapplication/Job/JobService.java
+++ src/main/java/com/app/jobapplication/Job/JobService.java
@@ -1,20 +1,21 @@
 package com.app.jobapplication.Job;
 
-// import org.jspecify.annotations.Nullable;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-
 public class JobService {
     private final JobRepository jobRepository;
 
-
-    public JobResponse getAllJobs() {
-       JobRepository.findAll().stream()
-       .map(JobResponse::fromEntity)
-       .collect(Collectors.toList);
-        
+    public List<JobResponse> getAllJobs() {
+        return jobRepository.findAll()
+                .stream()
+                .map(JobResponse::fromEntity)
+                .collect(Collectors.toList());
     }
-    
 }
