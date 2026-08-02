380c67fe441b11bdb215cd7b89d9eb4b4a35ac91
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-02 14:34:46 +0300

    Add GET endpoint to JobController for retrieving job by ID

--- src/main/java/com/app/jobapplication/Job/JobController.java
+++ src/main/java/com/app/jobapplication/Job/JobController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,9 @@ public class JobController {
     @PostMapping
     public ResponseEntity<JobResponse> addNewJob(@RequestBody JobRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(jobService.addNewJob(request));
+    }
+    @GetMapping("/id")
+    public ResponseEntity<JobResponse> getJobId(@PathVariable Long id){
+        return ResponseEntity.ok(jobService.getJobById());
     }
 }
