952854edad2f9f8d39f677f207838158f7bdea51
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-04 12:21:53 +0300

    Add CompanyRequest class to encapsulate company data for requests

---
+++ src/main/java/com/app/jobapplication/Company/dto/CompanyRequest.java
@@ -0,0 +1,10 @@
+package com.app.jobapplication.Company.dto;
+
+import lombok.Data;
+
+@Data
+public class CompanyRequest {
+    private String name;
+    private String location;
+    private String description;
+}
