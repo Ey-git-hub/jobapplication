822c1a24f633c85e2f5da23904ec83a50c6b7ccf
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-04 12:58:01 +0300

    Add updateCompany endpoint to CompanyController for company updates

--- src/main/java/com/app/jobapplication/Company/controller/CompanyController.java
+++ src/main/java/com/app/jobapplication/Company/controller/CompanyController.java
@@ -1,8 +1,11 @@
 package com.app.jobapplication.Company.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.app.jobapplication.Company.dto.CompanyRequest;
 import com.app.jobapplication.Company.service.Impl.CompanyServiceImpl;
 import com.app.jobapplication.Company.dto.CompanyResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,5 +20,14 @@ public class CompanyController {
   @GetMapping
   public ResponseEntity<List<CompanyResponse>> getAllComapnies() {
     return ResponseEntity.ok(companyServiceImpl.getAllCompanies());
+  }
+  @PostMapping("/update/{id}")
+  public ResponseEntity<String> updateCompany(@PathVariable Long id, @RequestBody CompanyRequest companyRequest) {
+    boolean isUpdated = companyServiceImpl.updateCompany(id, companyRequest);
+    if (isUpdated) {
+        return ResponseEntity.ok("Company updated successfully.");
+    } else {
+        return ResponseEntity.notFound().build();
+    }
   }
 }
