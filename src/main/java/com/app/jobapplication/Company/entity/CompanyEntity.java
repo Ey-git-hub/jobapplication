db464fa52dad48770ce3f2e4d16bd058b54d6400
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-04 12:22:13 +0300

    Add CompanyEntity class to represent company data in the application

---
+++ src/main/java/com/app/jobapplication/Company/entity/CompanyEntity.java
@@ -0,0 +1,29 @@
+package com.app.jobapplication.Company.entity;
+
+import java.util.List;
+
+import com.app.jobapplication.Job.entity.JobEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompanyEntity {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    @OneToMany
+    private List<JobEntity> jobs;
+    private String description;
+}
