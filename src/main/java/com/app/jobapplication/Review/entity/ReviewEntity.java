3eb29564451a4e51310c42a3b3360e01b51f14b1
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:49:52 +0300

    Add ReviewEntity class with fields for review details and annotations

--- src/main/java/com/app/jobapplication/Review/entity/ReviewEntity.java
+++ src/main/java/com/app/jobapplication/Review/entity/ReviewEntity.java
@@ -1,5 +1,24 @@
 package com.app.jobapplication.Review.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import lombok.*;
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String reviewerName;
+    private String reviewText;
+    private int rating;
+    private String jobName;
+    private String companyName;
+    private String reviewerEmail;
+    private String reviewerPhone;
 }
