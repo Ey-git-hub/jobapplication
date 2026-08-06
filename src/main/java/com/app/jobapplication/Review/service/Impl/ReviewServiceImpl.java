45bd928a6865404b7bedf7842e1d366104cbba02
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:50:27 +0300

    Refactor ReviewServiceImpl to include necessary imports and annotations

--- src/main/java/com/app/jobapplication/Review/service/Impl/ReviewServiceImpl.java
+++ src/main/java/com/app/jobapplication/Review/service/Impl/ReviewServiceImpl.java
@@ -1,5 +1,11 @@
 package com.app.jobapplication.Review.service.Impl;
 
+import org.springframework.stereotype.Service;
+import com.app.jobapplication.Review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewServiceImpl {
-    
+   private final ReviewRepository reviewRepository;
 }
