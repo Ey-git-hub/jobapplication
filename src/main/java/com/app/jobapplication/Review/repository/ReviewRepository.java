12e2d68aba9cdc77e3a70a83d8ef947545fd2e43
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:50:06 +0300

    Fix ReviewRepository to extend JpaRepository for ReviewEntity

--- src/main/java/com/app/jobapplication/Review/repository/ReviewRepository.java
+++ src/main/java/com/app/jobapplication/Review/repository/ReviewRepository.java
@@ -1,5 +1,8 @@
 package com.app.jobapplication.Review.repository;
 
-public class ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.app.jobapplication.Review.entity.ReviewEntity;
+
+public interface ReviewRepository extends JpaRepository<ReviewEntity,Long>{
     
 }
