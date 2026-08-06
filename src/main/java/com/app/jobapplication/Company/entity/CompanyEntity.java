d7d52c8202c60a971e82b8517c3d223ffa9db120
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:01:55 +0300

    Refactor CompanyEntity to clean up imports and re-enable annotations for constructors

--- src/main/java/com/app/jobapplication/Company/entity/CompanyEntity.java
+++ src/main/java/com/app/jobapplication/Company/entity/CompanyEntity.java
@@ -3,15 +3,15 @@ package com.app.jobapplication.Company.entity;
 import java.util.List;
 
 import com.app.jobapplication.Job.entity.JobEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-// import lombok.AllArgsConstructor;
 import lombok.*;
-// import lombok.NoArgsConstructor;
+
 
 @Getter
 @Setter
@@ -24,6 +24,7 @@ public class CompanyEntity {
     private Long id;
     private String name;
     private String location;
+    @JsonIgnore
     @OneToMany(mappedBy = "company")
     private List<JobEntity> jobs;
     private String description;
