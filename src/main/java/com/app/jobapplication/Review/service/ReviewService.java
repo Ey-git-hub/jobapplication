5798d9f2d94e66266cf7695a8fa53bf6b43b39f7
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:50:17 +0300

    Refactor ReviewService to define interface with review management methods

--- src/main/java/com/app/jobapplication/Review/service/ReviewService.java
+++ src/main/java/com/app/jobapplication/Review/service/ReviewService.java
@@ -1,5 +1,12 @@
 package com.app.jobapplication.Review.service;
 
-public class ReviewService {
-    
+import com.app.jobapplication.Review.dto.*;
+import java.util.List;
+
+public interface ReviewService {
+     List<ReviewResponse> getAllReviews();
+     boolean updateReview(Long id, ReviewRequest reviewRequest);
+     Void createReview(ReviewRequest reviewRequest);
+     boolean deleteReview(Long id);
+     ReviewResponse getReviewById(Long id);
 }
