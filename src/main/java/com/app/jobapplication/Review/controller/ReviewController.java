fc936fdf8c5220075aa873cb10c42bf1ef6e7ef3
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:48:41 +0300

    Add ReviewController with REST annotations and service integration

--- src/main/java/com/app/jobapplication/Review/controller/ReviewController.java
+++ src/main/java/com/app/jobapplication/Review/controller/ReviewController.java
@@ -1,5 +1,16 @@
 package com.app.jobapplication.Review.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.jobapplication.Review.service.Impl.ReviewServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
+    private final ReviewServiceImpl reviewService;
     
 }
