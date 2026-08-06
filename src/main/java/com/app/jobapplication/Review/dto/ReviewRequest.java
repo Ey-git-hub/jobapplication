67c0ac52a070b999e927e7cc924969cdb781ad9c
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-06 21:48:53 +0300

    Refactor ReviewRequest DTO to include additional fields for reviewer details

--- src/main/java/com/app/jobapplication/Review/dto/ReviewRequest.java
+++ src/main/java/com/app/jobapplication/Review/dto/ReviewRequest.java
@@ -1,5 +1,14 @@
 package com.app.jobapplication.Review.dto;
 
+import lombok.Data;
+
+@Data
 public class ReviewRequest {
-    
+     private String reviewerName;
+    private String reviewText;
+    private int rating;
+    private String jobName;
+    private String companyName;
+    private String reviewerEmail;
+    private String reviewerPhone;
 }
