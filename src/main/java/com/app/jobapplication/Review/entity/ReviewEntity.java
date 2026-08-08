9e999fe2636e53f2091ba3d432275fea53dbb093
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-08 10:28:29 +0300

    Refactor ReviewEntity to consolidate fields and improve structure

--- src/main/java/com/app/jobapplication/Review/entity/ReviewEntity.java
+++ src/main/java/com/app/jobapplication/Review/entity/ReviewEntity.java
@@ -2,7 +2,10 @@ package com.app.jobapplication.Review.entity;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import com.app.jobapplication.Company.entity.CompanyEntity;
+
 import jakarta.persistence.Entity;
 import lombok.*;
 @Getter
@@ -14,11 +17,10 @@ public class ReviewEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String reviewerName;
-    private String reviewText;
-    private int rating;
-    private String jobName;
-    private String companyName;
-    private String reviewerEmail;
-    private String reviewerPhone;
+    private String title;
+    private String description;
+    private double rating;
+    @ManyToOne
+    private CompanyEntity company;
+  
 }
