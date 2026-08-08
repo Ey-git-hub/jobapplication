b4037a33eeeb873d536d425a98fc2951b02111b4
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-08 10:28:48 +0300

    Refactor ReviewRepository to add method for retrieving reviews by company ID

--- src/main/java/com/app/jobapplication/Review/repository/ReviewRepository.java
+++ src/main/java/com/app/jobapplication/Review/repository/ReviewRepository.java
@@ -1,8 +1,15 @@
 package com.app.jobapplication.Review.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.jobapplication.Review.dto.ReviewResponse;
 import com.app.jobapplication.Review.entity.ReviewEntity;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity,Long>{
+
+    List<ReviewResponse> findByCompanyEntityId(Long id);
     
 }
