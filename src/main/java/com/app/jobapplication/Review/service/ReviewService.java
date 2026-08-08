3d7f5cd26a1e33ce1c67e481ea6a83cdeaebca68
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-08 10:29:03 +0300

    Refactor ReviewService to update method signatures for retrieving reviews by company ID

--- src/main/java/com/app/jobapplication/Review/service/ReviewService.java
+++ src/main/java/com/app/jobapplication/Review/service/ReviewService.java
@@ -4,9 +4,9 @@ import com.app.jobapplication.Review.dto.*;
 import java.util.List;
 
 public interface ReviewService {
-     List<ReviewResponse> getAllReviews();
-     boolean updateReview(Long id, ReviewRequest reviewRequest);
-     Void createReview(ReviewRequest reviewRequest);
-     boolean deleteReview(Long id);
-     ReviewResponse getReviewById(Long id);
+     List<ReviewResponse> getAllReviews(Long companyId);
+     // boolean updateReview(Long id, ReviewRequest reviewRequest);
+     boolean createReview(ReviewRequest reviewRequest,Long id);
+     // boolean deleteReview(Long id);
+     // ReviewResponse getReviewById(Long id);
 }
